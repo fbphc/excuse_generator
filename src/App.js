6f52118a5f9a78b0c96e0aec53231596ccd867f3
str_hc <fbpetrella@gmail.com>
2022-07-13 11:59:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [excuse, SetExcuse] = useState("Sorry but...");
+  const [excuseText, setExcuseText] = useState("");
+
+  const dataset = [
+    "Wifi problem",
+    "No electricity",
+    "I'm sick",
+    "Bathroom-break",
+    "The meeting is not working",
+    "Clashing-meetings",
+    "I didn't know",
+    "Timezone differences",
+    "Didn't get the invite",
+    "Blame it on the family/the pets /the spouse/the boogie",
+  ];
+
+  function dajeForte() {
+    const randomNumber = Math.floor(Math.random() * dataset.length);
+    SetExcuse(dataset[randomNumber]);
+  }
+  function addTheExcuse(e) {
+    e.preventDefault();
+    const isInAlready = dataset.some((excuse) => excuse === excuseText);
+    if (isInAlready)
+      return window.alert("Already in the dataset, try something else...");
+    if (excuseText === "") return window.alert("Add a valid excuse!");
+    dataset.push(excuseText);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container w-75">
+      <h1 className="container mt-5 mb-3 text-center">Excuses Generator</h1>
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <Button className="my-3" variant="primary" onClick={dajeForte}>
+          Generate
+        </Button>
+        <p className="h2 text-center" >{excuse}</p>
+      </div>
+      <form onSubmit={addTheExcuse} className="container my-5 d-flex justify-content-center align-items-center flex-column">
+        <label className="my-1" htmlFor="Add">Suggest an excuse:</label>
+        <input className="my-1"
+          type="text"
+          id="add"
+          name="Add"
+          placeholder="Your excuse..."
+          onChange={(e) => setExcuseText(e.target.value)}
+        />
+        <Button className="my-1" variant="primary" type="submit">
+          Add your excuse
+        </Button>
+      </form>
+      </div>
     </div>
   );
 }
