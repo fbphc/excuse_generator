c733a3207ac6d23c71e546b2c6a1a843afdf9fe7
str_hc <fbpetrella@gmail.com>
2022-08-09 14:16:17 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-/* eslint-disable */ 
+/* eslint-disable */
 import "./App.css";
-import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-const dataset = [
-  "Wifi problem",
-  "No electricity",
-  "I'm sick",
-  "Bathroom-break",
-  "The meeting is not working",
-  "Clashing-meetings",
-  "I didn't know",
-  "Timezone differences",
-  "Didn't get the invite",
-  "Blame it on the family/the pets /the spouse/the boogie",
-];
-
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import { dataset } from "./dataset.js";
 
 function App() {
   const [excuse, SetExcuse] = useState("Sorry but...");
   const [excuseText, setExcuseText] = useState("");
+  const [userEx, setUserEx] = useState([]);
 
- 
   function dajeForte() {
     const randomNumber = Math.floor(Math.random() * dataset.length);
     SetExcuse(dataset[randomNumber]);
@@ -32,31 +20,51 @@ function App() {
       return window.alert("Already in the dataset, try something else...");
     if (excuseText === "") return window.alert("Add a valid excuse!");
     dataset.push(excuseText);
+    setUserEx([...userEx, excuseText]);
+    e.target.reset();
   }
 
   return (
     <div className="App">
       <div className="main-container w-75">
-      <h1 className="container mt-5 mb-3 text-center">Excuses Generator</h1>
-      <div className="d-flex justify-content-center align-items-center flex-column">
-        <Button className="my-3" variant="primary" onClick={dajeForte}>
-          Generate
-        </Button>
-        <p className="h2 text-center" >{excuse}</p>
-      </div>
-      <form onSubmit={addTheExcuse} className="container my-5 d-flex justify-content-center align-items-center flex-column">
-        <label className="my-1" htmlFor="Add">Suggest an excuse:</label>
-        <input className="my-1"
-          type="text"
-          id="add"
-          name="Add"
-          placeholder="Your excuse..."
-          onChange={(e) => setExcuseText(e.target.value)}
-        />
-        <Button className="my-1" variant="primary" type="submit">
-          Add your excuse
-        </Button>
-      </form>
+        <h1 className="container mt-5 mb-2 text-center">Excuses Generator</h1>
+        <div className="d-flex justify-content-center align-items-center flex-column">
+          <Button className="my-3" variant="primary" onClick={dajeForte}>
+            Generate
+          </Button>
+          <p className="h2 text-center excuse">{excuse}</p>
+        </div>
+        <form
+          onSubmit={addTheExcuse}
+          className="container my-1 d-flex justify-content-center align-items-center flex-column"
+        >
+          <label className="my-1" htmlFor="Add">
+            Suggest an excuse:
+          </label>
+          <input
+            className="my-1 input"
+            type="text"
+            id="add"
+            name="Add"
+            placeholder="Your excuse..."
+            onChange={(e) => setExcuseText(e.target.value)}
+          />
+          <Button className="my-1" variant="primary" type="submit">
+            Add your excuse
+          </Button>
+        </form>
+        <div className="text-center mt-4 mb-3 mx-auto w-75">
+          <h4>Our Excuses:</h4>
+          {dataset.map((item) => (
+            <h5 className="d-inline">{item} / </h5>
+          ))}
+
+          <h4 className="mt-2 usEx">Your Excuses:</h4>
+          {userEx.map((item) => (
+            <h5 className="d-inline">{item} / </h5>
+          ))}
+        </div>
+        <div className="line mt-3 mb-5 w-25 mx-auto"></div>
       </div>
     </div>
   );
---
+++ src/dataset.js
@@ -0,0 +1,14 @@
+export const dataset = [
+    "Wi-fi problem",
+    "No electricity",
+    "I'm sick",
+    "Bathroom-break",
+    "The meeting is not working",
+    "Clashing-meetings",
+    "I didn't know",
+    "Timezone differences",
+    "Didn't get the invite",
+    "Blame it on the family",
+    "I can't, my cam is not working",
+    "I'll be right back, my cat needs me"
+  ];
